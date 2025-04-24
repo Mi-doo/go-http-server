272aa1598d0107df4b4221f0e6a187abdf1cdbb1
MAC <mehdighachoui@gmail.com>
2025-04-24 23:47:16 +0200

    files/post

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"os/exec"
 	"strings"
 )
 
@@ -17,12 +18,18 @@ func main() {
 
 	defer l.Close()
 
+	// var wg sync.WaitGroup
+
+	// for i := 1; i <= 3; i++ {
+	// wg.Add(1)
 	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
 
+	// go func() {
+	// defer wg.Done()
 	req := make([]byte, 1024)
 
 	if _, err := conn.Read(req); err != nil {
@@ -32,7 +39,7 @@ func main() {
 	data := string(req)
 	path := strings.Split(data, " ")[1]
 	response := ""
-
+	fmt.Println(data)
 	if path == "/" {
 		response = "HTTP/1.1 200 OK\r\n\r\n"
 	} else if strings.HasPrefix(path, "/echo") {
@@ -42,9 +49,30 @@ func main() {
 		agent := strings.Split(data, "\r\n")[3]
 		agent = strings.Split(agent, " ")[1]
 		response = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(agent), agent)
+	} else if strings.HasPrefix(path, "/files/") {
+		if strings.HasPrefix(data, "GET") {
+			text, err := os.ReadFile("./tmp/" + path[6:])
+			if err != nil {
+				response = "HTTP/1.1 404 NOT FOUND\r\n\r\n"
+			} else {
+				response = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(text), text)
+			}
+		} else if strings.HasPrefix(data, "POST") {
+			body := strings.Split(data, "\r\n\r\n")[1]
+
+			cmd := exec.Command("echo", "-n", body, ">", "./tmp/"+path[6:])
+			_, err := cmd.Output()
+			if err != nil {
+				fmt.Println(err.Error())
+			}
+		}
 	} else {
 		response = "HTTP/1.1 404 NOT FOUND\r\n\r\n"
 	}
 
 	conn.Write([]byte(response))
+	// }()
+	// }
+
+	// wg.Wait()
 }
